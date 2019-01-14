be5a841adab85f3367d6cc95d108dc23fab26e4c
R Res <clonnes@hotmail.com>
2019-01-14 11:18:27 -0500

    Add project files.

---
+++ HelloWorld_B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
