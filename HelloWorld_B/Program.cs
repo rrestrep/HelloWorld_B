313d8f53112b6c0e94deb7cc48611fd695d9f44a
R Res <clonnes@hotmail.com>
2019-01-15 19:07:58 -0500

    3

--- HelloWorld_B/Program.cs
+++ HelloWorld_B/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld_B
     {
         static void Main(string[] args)
         {
-            //Pause to Display Hello World
+            //Pause to Display Hello World 3
             Console.WriteLine("Hello World!");
             Console.WriteLine("Rafael Commands YOU -  to Press any Key to continue Second Version");
             Console.ReadKey(true);
